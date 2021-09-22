d92f69ac43abd8b820c2f4cfc0be2319805bc49a
harish091 <43706624+harish091@users.noreply.github.com>
2021-09-22 23:24:20 +0530

    done

---
+++ certificate.js
@@ -0,0 +1,47 @@
+<div className="certificate-container">
+    <div className="certificate">
+        <div className="water-mark-overlay"></div>
+        <div className="certificate-header">
+            <img src="https://rnmastersreview.com/img/logo.png" className="logo" alt=""></img>
+        </div>
+        <div className="certificate-body">
+           
+            <p className="certificate-title"><strong>TrainMe Application</strong></p>
+            <h1>Certificate of Completion</h1>
+            <p className="student-name">Hareesh Uppunuthula</p>
+            <div className="certificate-content">
+                <div className="about-certificate">
+                    <p>
+                has completed [______] hours on topic title here online on Date [________]
+                </p>
+                </div>
+                <p className="topic-title">
+                    The Topic consists of [hours] Continuity hours and includes the following:
+                </p>
+                <div className="text-center">
+                </div>
+            </div>
+            <div className="certificate-footer text-muted">
+                <div className="row">
+                    <div className="col-md-6">
+                        <p>Manager: ______________________</p>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <p>
+                                    Accredited by
+                                </p>
+                            </div>
+                            <div className="col-md-6">
+                                <p>
+                                    Endorsed by
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
